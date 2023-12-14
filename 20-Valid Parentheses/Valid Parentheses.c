74d12f9c54f395670e8834d55786ec1920ae3485
Levi <81131473+HsiangTW1031@users.noreply.github.com>
2023-12-14 06:37:53 +0000

    upload code-Valid Parentheses

--- 20-Valid Parentheses/Valid Parentheses.c
+++ 20-Valid Parentheses/Valid Parentheses.c
@@ -1,3 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAXSTACKSIZE 100
+char stack[MAXSTACKSIZE];
+int top = -1;
+
+int isEmpty();
+int push(char* stack);
+char pop();
+
 bool isValid(char* s) {
-    
+    int s_len = strlen(s);
+    int result;
+    int fail = 0;
+    if(s_len % 2 != 0) return false;
+
+    for (int i = 0; i < s_len; i++){
+        if(push(s)) fail = 1;
+    }
+
+    if(fail)
+        return false;
+    else
+        return true;
+}
+
+int push(char* s) {
+    if(top == MAXSTACKSIZE - 1){
+        printf("stack is full\n");
+    }else if(*s == '(' || *s == '{' || *s == '['){
+        stack[++top] = *s;
+    }else if(*s == ')'){
+        if(top == -1 || stack[top] == '(')
+            return 0;
+        else
+           top--;
+    }else if(*s == '}'){
+        if(top == -1 || stack[top] == '{')
+            return 0;
+        else
+           top--;
+    }else if(*s == '}'){
+        if(top == -1 || stack[top] == '[')
+            return 0;
+        else
+           top--;
+    }
+    s++;
+    return 1;
 }
