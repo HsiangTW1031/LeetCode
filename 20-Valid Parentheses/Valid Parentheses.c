bd6dbc150313e0d2dd6b5b68a2043c7bf97adbf9
Levi <81131473+HsiangTW1031@users.noreply.github.com>
2023-12-14 10:50:28 +0000

    updata code-remove pop function

--- 20-Valid Parentheses/Valid Parentheses.c
+++ 20-Valid Parentheses/Valid Parentheses.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define MAXSTACKSIZE 6
+#define MAXSTACKSIZE 100
 char stack[MAXSTACKSIZE];
 int top = -1;
 
 int push(char* stack);
-char pop();
 
 bool isValid(char* s) {
     int s_len = strlen(s);
     int result;
     int fail = 0;
-    if(s_len % 2 != 0) return false;
-
-    for (int i = 0; i < s_len; i++){
-        if(push(s)) fail = 1;
+    if(s_len % 2 != 0){
+        return false;
+    }else{
+        for (int i = 0; i < s_len; i++){
+            if(push(s)) {fail = 1;}
+        }
     }
-
     if(fail)
         return false;
     else
@@ -26,26 +26,26 @@ bool isValid(char* s) {
 }
 
 int push(char* s) {
-    if(top == MAXSTACKSIZE - 1){
-        printf("stack is full\n");
-    }else if(*s == '(' || *s == '{' || *s == '['){
+    if(*s == '(' || *s == '{' || *s == '['){
         stack[++top] = *s;
+        printf("%d",top);
+        printf("%c",stack[++top]);
     }else if(*s == ')'){
-        if(top == -1 || stack[top] == '(')
-            return 0;
+        if(top == -1 || stack[top] != '(')
+            return 1;
         else
            top--;
     }else if(*s == '}'){
-        if(top == -1 || stack[top] == '{')
-            return 0;
+        if(top == -1 || stack[top] != '{')
+            return 1;
         else
            top--;
-    }else if(*s == '}'){
-        if(top == -1 || stack[top] == '[')
-            return 0;
+    }else if(*s == ']'){
+        if(top == -1 || stack[top] != '[')
+            return 1;
         else
            top--;
     }
     s++;
-    return 1;
+    return 0;
 }
