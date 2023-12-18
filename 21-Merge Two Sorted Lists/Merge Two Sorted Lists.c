0dab96aea608f1a5b43e14333865dcdc6bc8b95d
Levi <81131473+HsiangTW1031@users.noreply.github.com>
2023-12-18 09:03:39 +0000

    Update code-Merge Two Sorted Lists

--- 21-Merge Two Sorted Lists/Merge Two Sorted Lists.c
+++ 21-Merge Two Sorted Lists/Merge Two Sorted Lists.c
@@ -6,5 +6,10 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode* temp;
+    struct ListNode* head;
     
+    if(!list1) return list2;
+    if(!list2) return list1;
+
 }
