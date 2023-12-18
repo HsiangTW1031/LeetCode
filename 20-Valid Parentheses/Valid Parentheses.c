cea7a6f3a5bb29ec69184e0986957a746af6cf07
Levi <81131473+HsiangTW1031@users.noreply.github.com>
2023-12-18 06:48:25 +0000

    upload code-Valid Parentheses

--- 20-Valid Parentheses/Valid Parentheses.c
+++ 20-Valid Parentheses/Valid Parentheses.c
@@ -2,50 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define MAXSTACKSIZE 100
+#define MAXSTACKSIZE 10000
 char stack[MAXSTACKSIZE];
-int top = -1;
-
-int push(char* stack);
 
 bool isValid(char* s) {
     int s_len = strlen(s);
-    int result;
-    int fail = 0;
+    char stack[MAXSTACKSIZE];
+    int top=-1;
+
     if(s_len % 2 != 0){
         return false;
     }else{
-        for (int i = 0; i < s_len; i++){
-            if(push(s)) {fail = 1;}
+        for (int i = 0; i < s_len; ++i){
+            if(*s == '(' || *s == '[' || *s == '{'){
+            stack[++top] = *s;
+            }else if((*s == ')' || *s == ']' || *s == '}') && top==-1){
+                return false;
+            }else if(*s == ')'){
+                if(stack[top]!= '('){
+                    top=-1;
+                    return false;
+                }
+                else top--;
+            }else if(*s == ']'){
+                if(stack[top]!= '['){
+                    top=-1;
+                    return false;
+                }
+                else top--;
+            }else if(*s == '}'){
+                if(stack[top]!= '{'){
+                    top=-1;
+                    return false;
+                }
+                else top--;
+            }
+            s++;
         }
     }
-    if(fail)
+
+    if(top != -1)
         return false;
     else
         return true;
-}
-
-int push(char* s) {
-    if(*s == '(' || *s == '{' || *s == '['){
-        stack[++top] = *s;
-        printf("%d",top);
-        printf("%c",stack[++top]);
-    }else if(*s == ')'){
-        if(top == -1 || stack[top] != '(')
-            return 1;
-        else
-           top--;
-    }else if(*s == '}'){
-        if(top == -1 || stack[top] != '{')
-            return 1;
-        else
-           top--;
-    }else if(*s == ']'){
-        if(top == -1 || stack[top] != '[')
-            return 1;
-        else
-           top--;
-    }
-    s++;
-    return 0;
 }
